e22e73e7704081926074e7ba59f2ff613bd7f48c
ltanyan <lus.tanyan03@gmail.com>
2024-05-06 23:29:15 +0400

    first commit

--- cypress/e2e/searchTest.cy.js
+++ cypress/e2e/searchTest.cy.js
@@ -1,16 +1,18 @@
 import {homePage} from "../POM/homePage/homePage.js";
 
 describe('Search Results Page', () => {
+    beforeEach(() => {
+        cy.visit('https://www.themoviedb.org');
+    })
+
     it('Single Valid Search Result', () => {
         const movieTitle = "Pirates of the Caribbean: On Stranger Tides";
-        cy.visit('https://www.themoviedb.org');
         homePage.searchMovie(movieTitle);
         cy.get('div.title h2').should('contain', movieTitle);
     });
 
     it('No Results', () => {
         const movieTitle = "dfaereagtr";
-        cy.visit('https://www.themoviedb.org');
         homePage.searchMovie(movieTitle);
         cy.get('div.title h2').should('not.exist');
     });
